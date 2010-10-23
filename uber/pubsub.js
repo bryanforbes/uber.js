1df3a3f830240fc1d3ece308aa666f2fd0e511d0
Bryan Forbes <bryan@reigndropsfall.net>
2010-10-23 17:16:30 -0500

    Updated to work with RequireJS.

--- uber/pubsub.js
+++ uber/pubsub.js
@@ -1,22 +1,24 @@
-(function(uber){
-    uber._topics = {};
+require.def("uber/pubsub", ["uber", "uber/dispatcher"], (function(global){
+    return function(uber){
+        uber._topics = {};
 
-    function subscribe(topic, func){
-        var f = uber._topics[topic];
-        if(!f){
-            f = uber._topics[topic] = uber.createDispatcher();
-            f.add(null);
+        function subscribe(topic, func){
+            var f = uber._topics[topic];
+            if(!f){
+                f = uber._topics[topic] = uber.createDispatcher();
+                f.add(null);
+            }
+            return f.add(func);
         }
-        return f.add(func);
-    }
 
-    function publish(topic, args){
-        var f = uber._topics[topic];
-        if(f){
-            f.apply(this, args||[]);
+        function publish(topic, args){
+            var f = uber._topics[topic];
+            if(f){
+                f.apply(this, args||[]);
+            }
         }
-    }
 
-    uber.subscribe = subscribe;
-    uber.publish = publish;
-})(uber);
+        uber.subscribe = subscribe;
+        uber.publish = publish;
+    };
+})(this));
